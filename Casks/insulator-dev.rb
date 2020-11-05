e77d404ff9b784f534cb52e90229a933f743a6c9
Andrea Vinci <andrewinci@gmail.com>
2020-11-05 08:15:31 +0000

    Update insulator-dev from 0.1.3 to 0.1.38

--- Casks/insulator-dev.rb
+++ Casks/insulator-dev.rb
@@ -1,6 +1,6 @@
 cask "insulator-dev" do
-  version "0.1.3"
-  sha256 "7177147780a4e0704dd323be3318ae5cc3474fe275b291ebdda265bcba6293f5"
+  version "0.1.38"
+  sha256 "2f00294e8c01d348cb8aca637d9f1c13039d51d27b4cc35efe2c814572316d69"
 
   url "https://github.com/andrea-vinci/Insulator/releases/download/#{version}/insulator-mac.zip"
   appcast "https://github.com/andrea-vinci/insulator/releases.atom"
