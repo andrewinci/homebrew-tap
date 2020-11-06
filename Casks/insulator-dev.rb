ee1a66dc5271c661d7b59e7a6b80be77ee2aaa45
andrea-vinci <andrea-vinci@users.noreply.github.com>
2020-11-06 04:45:29 +0000

    Update insulator-dev from 0.1.63 to 0.1.70

--- Casks/insulator-dev.rb
+++ Casks/insulator-dev.rb
@@ -1,6 +1,6 @@
 cask "insulator-dev" do
-  version "0.1.63"
-  sha256 "fb087c9d029f98f87b40f7dc56af932e24a4ea6c041e866d2ceba2a92cc68b61"
+  version "0.1.70"
+  sha256 "bf4b684985c3ab9e585c4dea4c4a6ac4a7a66e44aedc2570c2d72e2b8bef1e61"
 
   url "https://github.com/andrea-vinci/Insulator/releases/download/#{version}/insulator-mac.zip"
   appcast "https://github.com/andrea-vinci/insulator/releases.atom"
