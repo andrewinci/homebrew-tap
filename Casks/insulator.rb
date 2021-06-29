356c9ed2e05436ba85f05dcc59ad8c11dcc77737
andrewinci <andrewinci@users.noreply.github.com>
2021-06-29 08:53:18 +0000

    Update insulator from 0.3.8 to 0.4.0

--- Casks/insulator.rb
+++ Casks/insulator.rb
@@ -1,6 +1,6 @@
 cask "insulator" do
-  version "0.3.8"
-  sha256 "198a72c5a8ae59d0874dc29b94345067ff1860e11e4e6be2fff7ddbdd8cffd83"
+  version "0.4.0"
+  sha256 "3a6e2285d052ee28cc481a4ad3b4cc4f0ff218d60dc768a5ac12f4ecaa949d72"
 
   url "https://github.com/andrewinci/Insulator/releases/download/#{version}/insulator-mac.zip"
   appcast "https://github.com/andrewinci/insulator/releases.atom"
