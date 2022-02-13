9366698741bd4ccc5a32d53980ef2b3c3a2b62aa
andrewinci <andrewinci@users.noreply.github.com>
2022-02-13 17:01:28 +0000

    Update insulator from 0.5.2 to 0.5.3

--- Casks/insulator.rb
+++ Casks/insulator.rb
@@ -1,6 +1,6 @@
 cask "insulator" do
-  version "0.5.2"
-  sha256 "27357e1d6afb2456da9f270149053b2e34d7548497e0b4e1e9e0b71551dfd0c3"
+  version "0.5.3"
+  sha256 "dd81716f35ba18eaf25fb7240249584b00d03cba685d914a94c09889f3eb23a5"
 
   url "https://github.com/andrewinci/Insulator/releases/download/#{version}/insulator-mac.zip"
   appcast "https://github.com/andrewinci/insulator/releases.atom"
